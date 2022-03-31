0bc83aa5b87acf3a1b469c8ff2013b360b6d7522
Akhil Eaga <aeaga@pawsey.org.au>
2022-03-31 10:10:14 +0800

    Simple change to test PR in bitbucket

--- api/views.py
+++ api/views.py
@@ -14,7 +14,6 @@ def getRoutes(request):
 
         {'POST': '/api/users/token'}, # this one is provided by django as a CBV, tokens have an expiration date
         {'POST': '/api/users/token/refresh'} # this one is used to stay logged in by accessing the refreshed token
-
     ]
 
     # the safe parameter tells JsonResponse that we can return back somethingo other than a python dictionary
